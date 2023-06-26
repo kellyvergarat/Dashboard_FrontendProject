319e3b01549164c03d0bb0fca7b246065023b1bc
Rafael Eduardo Melo Rodriguez <rafaeleduardomelorodriguez@gmail.com>
2023-06-26 16:57:20 -0400

    Dashboard Revenues

--- dashboard/src/Pages/Dashboard.js
+++ dashboard/src/Pages/Dashboard.js
@@ -6,7 +6,7 @@ import {
     DollarCircleOutlined,
 } from "@ant-design/icons";
 import { useEffect, useState } from "react";
-import { getOrders } from "../API";
+import { getOrders, getRevenue } from "../API";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
@@ -20,8 +20,8 @@ ChartJS.register(
 );
 
 const Dashboard = () => {
-    return (
-        <div>
+    return ( 
+            <Space sise ={20} direction="vertical" >
             <Typography.Title level={4}> Dashboard</Typography.Title>
             <Space direction="horizontal">
                 <DashboardCard
@@ -78,8 +78,9 @@ const Dashboard = () => {
             </Space>
             <Space>
                 <RecentOrders />
+                <DashboardChart/>
             </Space>
-        </div>
+            </Space> 
     );
 };
 //passing props
@@ -133,6 +134,36 @@ function RecentOrders() {
 }
 
 function DashboardChart() {
+
+    const [revenueData , setRevenueData] = useState({
+        labels:[],
+        datasets:[]
+    });
+
+    useEffect(() => {
+        getRevenue().then(res => {
+            const labels = res.carts.map((cart) => {
+                return `User-${cart.userId}`;
+            });
+            const data = res.carts.map((cart) => {
+                return cart.discountedTotal;
+            });
+
+            const dataSource =  {
+                labels,
+                datasets: [
+                  {
+                    label: "Revenue",
+                    data: data,
+                    backgroundColor: 'rgba(255, 0, 0, 1)',
+                  }
+                ],
+              };
+              
+              setRevenueData(dataSource);
+        });
+    }, []);
+    
     const options = {
         responsive: true,
         plugins: {
@@ -145,26 +176,12 @@ function DashboardChart() {
             },
         },
     };
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const data = {
-        labels,
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: labels.map(() => Math.random()*1000),
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-          {
-            label: 'Dataset 2',
-            data: labels.map(() => Math.random()*1000),
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-          },
-        ],
-      };
-
+    
     return (
-        <Bar options={options} data={data} />
-    )
+        <Card style={{width:500, height:250}}>
+            <Bar options={options} data={revenueData} />
+        </Card>
+    );
 }
 
 export default Dashboard;
