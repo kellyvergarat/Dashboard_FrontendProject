180b6a7fa6a136b0c5e51ee5fbe58cd6901d44ce
Rafael Eduardo Melo Rodriguez <rafaeleduardomelorodriguez@gmail.com>
2023-06-26 22:15:26 -0400

    Inventory

--- dashboard/src/Pages/Inventory.js
+++ dashboard/src/Pages/Inventory.js
@@ -1,9 +1,66 @@
-import {Typography} from "antd";
-const Inventory = () =>{
-    return(
-        <div>
-            <Typography.Title level={4}> Inventory</Typography.Title>
-            <Typography.Text>Inventory</Typography.Text>
-        </div>)
-}
+import { Avatar, Rate, Space, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getInventory } from "../API";
+import { Title } from "chart.js";
+
+const Inventory = () => {
+  const [loading, setLoading] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getInventory().then((res) => {
+      setDataSource(res.products);
+    });
+  }, []);
+
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title level={4}> Inventory</Typography.Title>
+      <Table
+        columns={[
+          {
+            title: "Thumbnail",
+            dataIndex: "thumbnail",
+            render: (link) => {
+              return <Avatar src={link} />;
+            },
+          },
+          {
+            title: "Name",
+            dataIndex: "title",
+          },
+          {
+            title: "Brand",
+            dataIndex: "brand",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Stock",
+            dataIndex: "stock",
+          },
+          {
+            title: "Rating",
+            dataIndex: "rating",
+            render: (rating) => {
+              return <Rate value={rating} allowHalf disabled />;
+            },
+          },
+          {
+            title: "Category",
+            dataIndex: "category",
+          },
+        ]}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 10,
+        }}
+      ></Table>
+    </Space>
+  );
+};
 export default Inventory;
